ece84450374901eab2de87eea5c3b74362dcfde7
Wajahath <unfliptechnologies@gmail.com>
2024-08-25 14:33:31 +0530

    update 9.5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@vueuse/nuxt", "@pinia/nuxt"],
+  modules: ["@vueuse/nuxt", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
 });
